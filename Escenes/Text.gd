f964c29f0dbc16f19b8647da1171b97c1cbbc0b1
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-04-20 20:13:42 +0200

    Caselles funcionen, es pot escriure.

---
+++ Escenes/Text.gd
@@ -0,0 +1,39 @@
+extends Label
+var ratoli = false
+
+func _ready():
+	pass
+func _process(delta):
+	escriure()
+	
+func escriure():
+	if ratoli == true:
+		if Input.is_action_just_pressed("1"):
+			set_text('1')
+		if Input.is_action_just_pressed("2"):
+			set_text('2')
+		if Input.is_action_just_pressed("3"):
+			set_text('3')
+		if Input.is_action_just_pressed("4"):
+			set_text('4')
+		if Input.is_action_just_pressed("5"):
+			set_text('5')
+		if Input.is_action_just_pressed("6"):
+			set_text('6')
+		if Input.is_action_just_pressed("7"):
+			set_text('7')
+		if Input.is_action_just_pressed("8"):
+			set_text('8')
+		if Input.is_action_just_pressed("9"):
+			set_text('9')	
+	
+
+func _on_MarginContainer2_mouse_entered():
+	print('in')
+	ratoli = true
+	
+
+
+func _on_MarginContainer2_mouse_exited():
+	print('out')
+	ratoli = false
