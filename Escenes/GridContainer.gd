32159a37305cf52f24ed8045b3727eb0843c43f3
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-04-22 10:53:19 +0200

    Creant el sudoku
    Comnçant agenerar numeros

--- Escenes/GridContainer.gd
+++ Escenes/GridContainer.gd
@@ -1,22 +1,52 @@
 extends GridContainer
 
+
+var files = []
+var columnes = []
 func _ready():
-	var a1 = get_node("Casella/MarginContainer/Text")
-	var a2 = get_node("Casella2/MarginContainer/Text")
-	var a3 = get_node("Casella3/MarginContainer/Text")
-	var a4 = get_node("Casella4/MarginContainer/Text")
-	var a5 = get_node("Casella5/MarginContainer/Text")
-	var a6 = get_node("Casella6/MarginContainer/Text")
-	var a7 = get_node("Casella7/MarginContainer/Text")
-	var a8 = get_node("Casella8/MarginContainer/Text")
-	var a9 = get_node("Casella9/MarginContainer/Text")
-	a1.text = 'c'
-	a2.text = 'c'
-	a3.text = 'c'
-	a4.text = 'c'
-	a5.text = 'c'
-	a6.text = 'c'
-	a7.text = 'c'
-	a8.text = 'c'
-	a9.text = 'c'
-	
+	var caselles = get_children()
+	for i in caselles:
+		print(i.name)
+	genera_sudoku()
+
+func genera_sudoku():	
+#	for i in range(9):
+#		files.append([])
+#		for j in range(9):
+#			files[i].append(j+1)
+#	print(files)
+#
+#	for i in range(9):
+#		columnes.append([])
+#		for j in files:
+#			columnes[i].append(j[0])
+#
+#	print(columnes)
+var base  = 3
+var side  = base*base
+
+# pattern for a baseline valid solution
+def pattern(r,c): return (base*(r%base)+r//base+c)%side
+
+# randomize rows, columns and numbers (of valid base pattern)
+from random import sample
+def shuffle(s): return sample(s,len(s)) 
+rBase = range(base) 
+rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
+cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+nums  = shuffle(range(1,base*base+1))
+
+# produce board using randomized baseline pattern
+board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+
+for line in board: print(line)
+
+[6, 2, 5, 8, 4, 3, 7, 9, 1]
+[7, 9, 1, 2, 6, 5, 4, 8, 3]
+[4, 8, 3, 9, 7, 1, 6, 2, 5]
+[8, 1, 4, 5, 9, 7, 2, 3, 6]
+[2, 3, 6, 1, 8, 4, 9, 5, 7]
+[9, 5, 7, 3, 2, 6, 8, 1, 4]
+[5, 6, 9, 4, 3, 2, 1, 7, 8]
+[3, 4, 2, 7, 1, 8, 5, 6, 9]
+[1, 7, 8, 6, 5, 9, 3, 4, 2]
--- Sudoku.gd
+++ Sudoku.gd
@@ -1,109 +1,109 @@
-#extends Node2D
-#
-#var fila = 9
-#var columna = 9
-#var tamany = 40
-#var Panel_scene = preload("res://Escenes/Panel.tscn")
-#var arr = []
-#func _ready():
-#	dins_graella()
-#	dins_arr()
-#	fill_numbers()
-#	show()
-#
-#func dins_graella():
-#	var distancia_x = 0
-#	var distancia_y = 0 
-#	for i in fila:
-#		distancia_x = 0
-#		distancia_y += 2
-#		if i % 3 == 0:
-#			distancia_y += 2
-#		for j in columna:
-#			distancia_x += 2
-#			if j % 3 == 0:
-#				distancia_x += 2
-#			var panel = Panel_scene.instance()
-#			panel.rect_position = Vector2(tamany * j,tamany * i) + Vector2(distancia_x, distancia_y)
-#			add_child(panel)
-#
-#func dins_arr():
-#	for i in fila:
-#		for j in columna:
-#			arr.append(null)
-#
-#func show():
-#	var panels = get_children()
-#	for i in fila:
-#		for j in columna:
-#			var numero = arr[i * fila + j]
-#			if numero:
-#				panels[i * fila + j].get_node("Label").text = str(numero)
-#
-#func fill_numbers():
-#	for n in 3:
-#		var numeros = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
-#		numeros.shuffle()
-#		for i in range(n * 3, n * 3 + 3):
-#			for j in range(n * 3, n * 3 + 3):
-#				arr[i * fila + j] = numeros.pop_back()
-#	fill_buits()
-#
-#func fill_buits():
-#	var find_null = arr.find(null)
-#
-#	if find_null == -1:
-#		return true
-#
-#	var i = find_null / fila
-#	var j = find_null % columna
-#
-#	var candidats = get_numeros_candidats(i, j)
-#
-#	if !candidats:
-#		return false
-#	var fill
-#
-#	while candidats:
-#		fill = candidats.pop_back()
-#		arr[i * fila + j] = fill
-#		if fill_buits():
-#			return true
-#		else:
-#			arr[i * fila + j] = null
-#func get_numeros_candidats(i , j):
-#	var numeros = [ 1, 2, 3, 4, 5, 6, 7 , 8, 9]
-#	for numero in get_fila_numbers(i):
-#		numeros.erase(numero)
-#	for numero in get_columna_numbers(j):
-#		numeros.erase(numero)
-#	for numero in get_area_numbers(i ,j):
-#		numeros.erase(numero)
-#	return numeros
-#
-#func get_fila_numbers(i):
-#	var resolucio = []
-#	for j in columna:
-#		var numero = arr[i * fila + j]
-#		if numero:
-#			resolucio.append(numero)
-#	return resolucio
-#
-#func get_columna_numbers(j):
-#	var resolucio = []
-#	for i in fila:
-#		var numero = arr[i * fila + j]
-#		if numero:
-#			resolucio.append(numero)
-#	return resolucio
-#
-#func get_area_numbers(i ,j):
-#	var distancia_i = i / 3 * 3
-#	var distancia_j = j / 3 * 3
-#	var resolucio = []
-#	for area_i in range(distancia_i, distancia_i + 3):
-#		for area_j in range(distancia_j, distancia_j + 3):
-#			var numero = arr[area_i * fila + area_j]
-#			if numero:
-#				resolucio.append(numero)
-#	return resolucio
+extends Node2D
+
+var fila = 9
+var columna = 9
+var tamany = 40
+var Panel_scene = preload("res://Escenes/Panel.tscn")
+var arr = []
+func _ready():
+	dins_graella()
+	dins_arr()
+	fill_numbers()
+	show()
+
+func dins_graella():
+	var distancia_x = 0
+	var distancia_y = 0 
+	for i in fila:
+		distancia_x = 0
+		distancia_y += 2
+		if i % 3 == 0:
+			distancia_y += 2
+		for j in columna:
+			distancia_x += 2
+			if j % 3 == 0:
+				distancia_x += 2
+			var panel = Panel_scene.instance()
+			panel.rect_position = Vector2(tamany * j,tamany * i) + Vector2(distancia_x, distancia_y)
+			add_child(panel)
+
+func dins_arr():
+	for i in fila:
+		for j in columna:
+			arr.append(null)
+
+func show():
+	var panels = get_children()
+	for i in fila:
+		for j in columna:
+			var numero = arr[i * fila + j]
+			if numero:
+				panels[i * fila + j].get_node("Label").text = str(numero)
+
+func fill_numbers():
+	for n in 3:
+		var numeros = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		numeros.shuffle()
+		for i in range(n * 3, n * 3 + 3):
+			for j in range(n * 3, n * 3 + 3):
+				arr[i * fila + j] = numeros.pop_back()
+	fill_buits()
+
+func fill_buits():
+	var find_null = arr.find(null)
+
+	if find_null == -1:
+		return true
+
+	var i = find_null / fila
+	var j = find_null % columna
+
+	var candidats = get_numeros_candidats(i, j)
+
+	if !candidats:
+		return false
+	var fill
+
+	while candidats:
+		fill = candidats.pop_back()
+		arr[i * fila + j] = fill
+		if fill_buits():
+			return true
+		else:
+			arr[i * fila + j] = null
+func get_numeros_candidats(i , j):
+	var numeros = [ 1, 2, 3, 4, 5, 6, 7 , 8, 9]
+	for numero in get_fila_numbers(i):
+		numeros.erase(numero)
+	for numero in get_columna_numbers(j):
+		numeros.erase(numero)
+	for numero in get_area_numbers(i ,j):
+		numeros.erase(numero)
+	return numeros
+
+func get_fila_numbers(i):
+	var resolucio = []
+	for j in columna:
+		var numero = arr[i * fila + j]
+		if numero:
+			resolucio.append(numero)
+	return resolucio
+
+func get_columna_numbers(j):
+	var resolucio = []
+	for i in fila:
+		var numero = arr[i * fila + j]
+		if numero:
+			resolucio.append(numero)
+	return resolucio
+
+func get_area_numbers(i ,j):
+	var distancia_i = i / 3 * 3
+	var distancia_j = j / 3 * 3
+	var resolucio = []
+	for area_i in range(distancia_i, distancia_i + 3):
+		for area_j in range(distancia_j, distancia_j + 3):
+			var numero = arr[area_i * fila + area_j]
+			if numero:
+				resolucio.append(numero)
+	return resolucio
