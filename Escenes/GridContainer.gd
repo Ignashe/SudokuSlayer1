bac9c555f28d9ee1e45ba6176f8250980277a7e3
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-04-25 16:57:17 +0200

    Gran millora, queden posar les condicions del 4*4

--- Escenes/GridContainer.gd
+++ Escenes/GridContainer.gd
@@ -1,12 +1,12 @@
 extends GridContainer
 
 var files = [] 
-
-
+var x = 0
+var y = 0
 func _ready():
 	var caselles = get_children()
-	for i in caselles:
-		print(i.name)
+#	for i in caselles:
+##		print(i.name)
 	genera_sudoku()
 	omplir_numeros()
 func genera_sudoku():	
@@ -14,7 +14,7 @@ func genera_sudoku():
 		files.append([])
 		for j in range(4):
 			files[i].append(0)
-	print(files)
+	
 
 func omplir_numeros():
 	for i in files:
@@ -25,8 +25,27 @@ func omplir_numeros():
 		var posicio = posicions[randi() % numeros.size()]
 		numeros.erase(primer_numero)
 		i[posicio] = primer_numero
-		print(numeros)
-	print(files)
 	
-
-	
+	for i in files:
+		for element in i:
+			if element == 0:
+				var numeros = [1,2,3,4]
+				if 1 in i:
+					numeros.erase(1) 
+				if 2 in i:
+					numeros.erase(2)
+				if 3 in i:
+					numeros.erase(3)
+				if 4 in i:
+					numeros.erase(4)
+				i[x] = numeros[randi() % numeros.size()]
+			print(x)
+			if x < 4:
+				x += 1
+			if x > 3:
+				x = 0
+			
+	print(files[0])
+	print(files[1])
+	print(files[2])
+	print(files[3])
