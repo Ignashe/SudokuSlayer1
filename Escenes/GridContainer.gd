ea137be1dcc47037d545cb3492079f271dfb538c
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-05-02 18:59:45 +0200

    Inicis en eliminar numeros

--- Escenes/GridContainer.gd
+++ Escenes/GridContainer.gd
@@ -2,9 +2,13 @@ extends GridContainer
 
 
 var sudoku = Sudoku.crea_sudoku()
-func _ready():
-	_escriure()
+var text = []
 
+func _ready():
+	
+#	_eliminar_num()
+	_escriure()
+	
 func _escriure():
 	var panels = get_children()
 	var x = 0
@@ -13,9 +17,13 @@ func _escriure():
 			for numero in i:
 				panels[x].get_node("MarginContainer/Text").text = str(numero)
 				x += 1
-
-
-
-
+func _eliminar_num():
+	for i in sudoku:
+		text.append(i)
 	
-	
+	for j in text:
+		var posibilitats = [0,1,2,3,4,5,6,7,8]
+		randomize()
+		for numero in j:
+			posibilitats.shuffle()
+			j[posibilitats[0]] = ''
