e6435051e75f7e34cc547a78e131e7cf4d756603
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-05-13 20:19:34 +0200

    CACA

--- Escenes/GridContainer.gd
+++ Escenes/GridContainer.gd
@@ -10,7 +10,7 @@ var sudoku_resolt = Sudoku.sudoku_resolt
 
 func _ready():
 	_escriure()
-	correcte()
+	sudoku_escrit()
 	
 func _escriure():
 	var panels = get_children()
@@ -26,7 +26,7 @@ func _escriure():
 				panels[x].get_node("MarginContainer/Text").columna = int(x / 9)
 				x += 1
 				
-func correcte():
+func sudoku_escrit():
 	var panels = get_children()
 	var x = 0
 	while x < 81:
@@ -35,9 +35,15 @@ func correcte():
 	
 func _on_casella_canviada(fila, columna, n, casella):
 		print('He canviat (%d, %d)' % [fila, columna])
+		var x = 0
 		var resolucio = sudoku_resolt[columna*9+fila]
 		var c = get_node(casella)
 		if str(resolucio) == n:
 			c.set_text(n)
 			sudoku_escrit[columna*9+fila] = n
-			
+			if sudoku_escrit:
+				for numero in sudoku_escrit:
+					if numero == '':
+						x += 1
+				if x == 0:
+					print('SOLUCIONAT') 
