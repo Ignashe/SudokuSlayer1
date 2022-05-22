23cdf1ea01c4e26b6e1b928314a3a639a555815a
Ignashe <101245846+Ignashe@users.noreply.github.com>
2022-05-22 17:29:10 +0200

    Gran millora

--- Escenes/GridContainer.gd
+++ Escenes/GridContainer.gd
@@ -7,15 +7,34 @@ var correcte = false
 var sudoku_escrit = []
 var sudoku_resolt = Sudoku.sudoku_resolt
 onready var timer = get_node('../Canvi de color')
+onready var timer_resposta = get_node('../Temps de resposta')
+onready var text_temps = get_tree().get_nodes_in_group('Temps')[0]
 var caselles_canviades = []
+var timer_on = false
+var time = 15
+
 
 func _ready():
-	Global.update_errors()
-	Global.update_vida()
+	timer_resposta.set_wait_time(15)
+	timer_resposta.start()
+	timer_on = true
 	timer.set_wait_time(0.25)
+	Global.update_vida()
+	Global.update_vida_enemic()
+	
+	
 	_escriure()
 	sudoku_escrit()
+
+func _process(delta):
+	if(timer_on):
+		time -= delta
+
+	var segons = fmod(time,60)
+	var mils = fmod(time,1)*100
+	var text = str('%02d : %02d' % [segons,mils])
 	
+	text_temps.set_text(var2str(text))
 func _escriure():
 	var panels = get_children()
 	var x = 0
@@ -41,12 +60,12 @@ func _error(casella,color):
 		color.modulate = Color(255,0,0)
 		caselles_canviades.append(color)
 		timer.start()
-		Global.errors += 1
-		Global.update_errors()
-		Global.vida -= 10
+		Global.vida -= 5
 		Global.update_vida()
+
 func _encert():
-		Global.vida_enemic -= 10
+		time = 15
+		Global.vida_enemic -= 2.5
 		Global.update_vida_enemic()
 		
 func _on_casella_canviada(fila, columna, n, casella, color):
@@ -72,3 +91,10 @@ func _on_Canvi_de_color_timeout():
 	for casella_canviada in caselles_canviades:
 		casella_canviada.modulate = Color(1,1,1)
 
+
+func _on_Temps_de_resposta_timeout():
+		Global.vida -= 5
+		Global.update_vida()
+		time = 15
+		timer_resposta.start()
+	
--- Global.gd
+++ Global.gd
@@ -1,13 +1,11 @@
 extends Node
 
 var errors = 0
-var vida = 30
-var vida_enemic = 250
+var vida = 50
+var vida_enemic = 50
 func _ready():
 	pass 
 
-func update_errors():
-	get_tree().get_nodes_in_group('errors')[0].text = str(errors)
 func update_vida():
 	get_tree().get_nodes_in_group('Vida')[0].value = vida
 func update_vida_enemic():
